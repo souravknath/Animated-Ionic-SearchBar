bd01a5d7e6b62dc61a82250701e0529b9db34ac6
souravnath <souravnath@SDRC.COM>
2019-12-23 17:33:26 +0530

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public isSearchBoxOpened:boolean = false
   constructor() {}
-
+  onSearch(event){
+    console.log(event);
+  }
 }
